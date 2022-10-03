8ae7aeb6fad43efd5700d40a1e853b440d9b2930
santiago <santiagotaschetta@gmail.com>
2022-10-03 14:42:41 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <header>
+    <h1>Weather App</h1>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+    </nav>
+  </header>
+  <RouterView />
+</template>
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
